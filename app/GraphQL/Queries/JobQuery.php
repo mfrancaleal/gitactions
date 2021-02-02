d4d1b737c2cc09f0a99673723dd991929dc44a37
Michel Leal <michel.leal@before.com.br>
2021-02-02 15:58:46 -0300

    Adding PhpStan

--- app/GraphQL/Queries/JobQuery.php
+++ app/GraphQL/Queries/JobQuery.php
@@ -1,17 +1,16 @@
 <?php
 
-
 namespace App\GraphQL\Queries;
 
-
 use App\Models\Job;
+use Illuminate\Support\Collection;
 
 class JobQuery
 {
-    public function byStatus($root, array $args)
+    public function byStatus(array $args): Collection
     {
         return Job::query()
-        ->where('status', $args['status'])
-        ->get();
+            ->where('status', $args['status'])
+            ->get();
     }
 }
